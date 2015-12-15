fb7c0e64d2cbabcace9aef8288dcbf430d2f4bbe
Gaetan SIMEON <gsimeon@student.42.fr>
2015-12-15 21:50:22 +0100

    maj memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gsimeon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/28 18:58:27 by gsimeon           #+#    #+#             */
-/*   Updated: 2015/12/08 18:35:19 by gsimeon          ###   ########.fr       */
+/*   Updated: 2015/12/15 21:48:25 by gsimeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,14 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*tmp;
-	size_t	i;
-
-	tmp = (char *)ft_memalloc(len + 1);
-	if (src == dst || len == 0)
-		return (dst);
-	i = len;
-	while (i--)
-		tmp[i] = *((char *)src + i);
-	while (len--)
-		*((char *)dst + len) = tmp[len];
+	if (dst < src)
+		ft_memcpy(dst, src, len);
+	else if (dst > src)
+	{
+		while (len--)
+		{
+			*((char *)dst + len) = *((char *)src + len);
+		}
+	}
 	return (dst);
 }
