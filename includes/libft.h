8cc04affa8eb597d87a5c7b97c7b1441ff7e783d
Gaetan SIMEON <gsimeon@student.42.fr>
2016-01-13 18:38:02 +0100

    MAJ includes

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: gsimeon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/30 13:01:19 by gsimeon           #+#    #+#             */
-/*   Updated: 2016/01/13 14:40:11 by gsimeon          ###   ########.fr       */
+/*   Updated: 2016/01/13 18:17:55 by gsimeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,12 @@ typedef struct		s_list
 	struct s_list	*next;
 }					t_list;
 
+/*
+**							MEMORY USAGE
+*/
+
+void				*ft_memalloc(size_t size);
+void				ft_memdel(void **ap);
 void				*ft_memset(void *b, int c, size_t len);
 void				ft_bzero(void *s, size_t n);
 void				*ft_memcpy(void *dst, const void *src, size_t n);
@@ -29,6 +35,11 @@ void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
 void				*ft_memchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
+
+/*
+**							STRINGS USAGE
+*/
+
 size_t				ft_strlen(const char *s);
 char				*ft_strdup(const char *s1);
 char				*ft_strcpy(char *dst, const char *src);
@@ -42,17 +53,6 @@ char				*ft_strstr(const char *s1, const char *s2);
 char				*ft_strnstr(const char *s1, const char *s2, size_t n);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
-int					ft_atoi(const char *str);
-int					ft_isalpha(int c);
-int					ft_isdigit(int c);
-int					ft_isalnum(int c);
-int					ft_isascii(int c);
-int					ft_isprint(int c);
-int					ft_toupper(int c);
-int					ft_tolower(int c);
-
-void				*ft_memalloc(size_t size);
-void				ft_memdel(void **ap);
 char				*ft_strnew(size_t size);
 void				ft_strdel(char **as);
 void				ft_strclr(char *s);
@@ -66,7 +66,27 @@ char				*ft_strsub(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s);
 char				**ft_strsplit(char const *s, char c);
-char				*ft_itoa(int n);
+char				**ft_boardset(int x, int y, char c);
+
+/*
+**							LIST USAGE
+*/
+
+t_list				*ft_lstnew(void const *content, size_t content_size);
+void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstadd(t_list **alst, t_list *new);
+void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+void				ft_lstend(t_list **lst, t_list *new);
+size_t				ft_lstinser(t_list **lst, t_list *src, size_t n);
+size_t				ft_lstlen(t_list *lst);
+t_list				*ft_lstshift(t_list **lst);
+
+/*
+**							DISPLAY USAGE
+*/
+
 void				ft_putchar(char c);
 void				ft_putstr(char const *s);
 void				ft_putendl(char const *s);
@@ -75,20 +95,21 @@ void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char const *s, int fd);
 void				ft_putendl_fd(char const *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
-
-t_list				*ft_lstnew(void const *content, size_t content_size);
-void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
-void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
-void				ft_lstadd(t_list **alst, t_list *new);
-void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
-t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
-
-void				ft_lstend(t_list **lst, t_list *new);
-size_t				ft_lstinser(t_list **lst, t_list *src, size_t n);
-size_t				ft_lstlen(t_list *lst);
-size_t				ft_atoi_base(const char *str, int base);
-t_list				*ft_lstshift(t_list **lst);
-char				**ft_boardset(int x, int y, char c);
 void				ft_boardprint(char **tab, int x, int y);
+
+/*
+**							OTHER USAGE
+*/
+
+int					ft_atoi(const char *str);
+char				*ft_itoa(int n);
+size_t				ft_atoi_base(const char *str, int base);
+int					ft_isalpha(int c);
+int					ft_isdigit(int c);
+int					ft_isalnum(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
+int					ft_toupper(int c);
+int					ft_tolower(int c);
 
 #endif
