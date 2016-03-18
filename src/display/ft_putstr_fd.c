1d03f554e102fada91f4adbbf1c79641df0de54d
Gaetan SIMEON <gsimeon@student.42.fr>
2016-03-18 12:45:05 +0100

    FIX : include unistd

--- src/display/ft_putstr_fd.c
+++ src/display/ft_putstr_fd.c
@@ -6,11 +6,12 @@
 /*   By: gsimeon <gsimeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 01:55:58 by gsimeon           #+#    #+#             */
-/*   Updated: 2016/03/18 12:32:15 by gsimeon          ###   ########.fr       */
+/*   Updated: 2016/03/18 12:44:20 by gsimeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <unistd.h>
 
 void	ft_putstr_fd(char const *s, int fd)
 {
